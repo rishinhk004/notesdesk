7e96d9df5286deebbc43f9827b17eb3bc2de4c7b
RISHI RAJ SAHA <rishir_ug_22@ece.nits.ac.in>
2024-06-14 15:02:55 +0530

    firebase init

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,14 @@
+import Login from "../../components/login/Login";
+import React, { useEffect, useState } from 'react';
+
 const Home = () => {
-    return <>Homepage</>
+    const token = localStorage.getItem('token');
+    const user = JSON.parse(localStorage.getItem('user'));
+    if(token){
+        return <h1>Hello, {user && user.displayName}</h1>
+    }else{
+        return <Login />
+    }
 }
 
 export default Home;
